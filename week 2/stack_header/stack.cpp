bb91f8c6e5c9a61884d49975c017fca35a3a3ff7
Khusanbek Gafurov <x.gafurov@gmail.com>
2024-10-13 20:45:06 +0500

    Add files via upload

---
+++ week 2/stack_header/stack.cpp
@@ -0,0 +1,47 @@
+
+#include <iostream> 
+#include "stack.h"
+
+using namespace std;
+
+bool Stack::push(int x) 
+{ 
+    if (top >= (MAX - 1)) { 
+        cout << "Stack Overflow"; 
+        return false; 
+    } 
+    else { 
+        a[++top] = x; 
+        cout << x << " pushed into stack\n"; 
+        return true; 
+    } 
+} 
+
+int Stack::pop() 
+{ 
+    if (top < 0) { 
+        cout << "Stack Underflow"; 
+        return 0; 
+    } 
+    else { 
+        int x = a[top--]; 
+        return x; 
+    } 
+} 
+
+int Stack::peek() 
+{ 
+    if (top < 0) { 
+        cout << "Stack is Empty"; 
+        return 0; 
+    } 
+    else { 
+        int x = a[top]; 
+        return x; 
+    } 
+} 
+
+bool Stack::isEmpty() 
+{
+    return (top < 0); 
+} 
