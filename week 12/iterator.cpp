ba1318572178529612d9b0e73df468967c065233
Khusanbek Gafurov <x.gafurov@gmail.com>
2024-11-18 02:08:36 +0500

    Add files via upload

---
+++ week 12/iterator.cpp
@@ -0,0 +1,27 @@
+// C++ program to show how to use iterator to
+// traverse vector container
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	
+	vector<int> arr = {1, 2, 3, 4, 5};
+
+	// Defining an iterator pointing to
+	// the beginning of the vector
+	vector<int>::iterator first = arr.begin();
+
+	// Defining an iterator pointing to the
+	// end of the vector
+	vector<int>::iterator last = arr.end();
+
+	// Iterating the whole vector
+	while(first != last) {
+		
+		cout << *first << " ";
+		first++;
+	}
+
+	return 0;
+}
